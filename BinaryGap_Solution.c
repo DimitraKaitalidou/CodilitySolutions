432d3fd2c1d3f13424bc4dca2600b9f86dd5d401
DimitraKaitalidou <49205589+DimitraKaitalidou@users.noreply.github.com>
2019-04-02 20:23:02 +0300

    [ADDED] added BinaryGap_Solution.c

---
+++ BinaryGap_Solution.c
@@ -0,0 +1,40 @@
+int solution(int N) {
+    
+    // Initialize parameters
+    int k;
+    int count[16]; // 32/2 = 16
+    int num = 0;
+    int start = 0;
+    int max = 0;
+    
+    for(int i = 0; i < 16; i++)
+       {
+           count[i] = 0;
+       }
+    
+    // Use binary shift to count the binary gaps
+    for(int j = 0; j < 31; j++)
+       {
+           k = N >> j;
+           
+           if(k & 1)
+              {
+                  start = 1;
+                  if(count[num] != 0) num++;
+              }
+              
+           else
+              if(start == 1) count[num]++;
+              if(j == 30) count[num] = 0;
+       }
+    
+    // Find the maximum binary gap
+    for(int l = 0; l <16; l++)
+       {
+           if(count[l] >= max) max = count[l];
+           
+       }
+       
+    return max;
+}
+
