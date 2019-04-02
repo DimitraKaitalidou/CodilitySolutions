c543d24c163a8b0fecb8d7bdf445d8a1d58fdea4
DimitraKaitalidou <49205589+DimitraKaitalidou@users.noreply.github.com>
2019-04-02 21:32:04 +0300

    [ADDED] added FrogJmp_Solution.c

---
+++ FrogJmp_Solution.c
@@ -0,0 +1,29 @@
+int solution(int X, int Y, int D) {
+    
+    // Initialize variables
+    int r_Y;
+	int r;
+    int jumps;
+	
+    r_Y = Y - X; // Calculate the remaining distance
+    
+	if(r_Y > D) // If the remaining distance is greater than D, divide the remaining distance with D
+	   {
+	      r = r_Y%D; 
+		  if(r != 0)
+		     {
+			    jumps = ((r_Y - r) / D) + 1;
+			 }
+		  else jumps = r_Y / D;
+		  
+	   }
+	else if(0 < r_Y && r_Y <= D) // If the remaining distance is less or equal to D, one jump is enough
+	   {
+	      jumps = 1;
+	   }
+	else jumps = 0; // If the remaining distance is zero, no jumps are required
+    
+    // Return result
+    return jumps;
+}
+
