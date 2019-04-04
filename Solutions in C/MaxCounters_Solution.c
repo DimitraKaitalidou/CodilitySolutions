22a2a533414eaf453ed341594e86507daa8c0eec
DimitraKaitalidou <49205589+DimitraKaitalidou@users.noreply.github.com>
2019-04-04 23:08:24 +0300

    [ADDED] added MaxCounters_Solution.c

---
+++ Solutions in C/MaxCounters_Solution.c
@@ -0,0 +1,37 @@
+struct Results solution(int N, int A[], int M) {
+    
+    // Initialize variables
+    struct Results result;
+    int *count = (int *)malloc(N * sizeof(int));
+    int max = 0;
+    int increase_all = 0;
+    
+    for(int k = 0; k < N; k++) count[k] = 0;
+                     
+    for(int i = 0; i < M; i++)
+       {
+           if(A[i] < N + 1) // If A[i] is less than N + 1, increase counter i and calculate the max counter value
+              {
+                 if(count[A[i] - 1] < increase_all) count[A[i] - 1] = increase_all;
+                 count[A[i] - 1]++; 
+                 if(count[A[i] - 1] > max) max = count[A[i] - 1];
+                 
+              }
+           else // If A[i] is equal to N + 1, save the current maximum counter value
+              {
+                  increase_all = max;
+                  
+              }
+       }
+       
+    for(int j = 0; j < N; j++)
+        {
+           if(count[j] < increase_all) count[j] = increase_all; // If counter j was not calculated, assign the last current maximum counter value
+        }
+    
+    // Return result                
+    result.C = count;
+    result.L = N;
+    return result;
+}
+
