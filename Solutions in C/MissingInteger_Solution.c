9946ce89a0df1645341a53d97e82a30450aea581
DimitraKaitalidou <49205589+DimitraKaitalidou@users.noreply.github.com>
2019-04-06 20:57:35 +0300

    [ADDED] added MissingInteger_Solution.c

---
+++ Solutions in C/MissingInteger_Solution.c
@@ -0,0 +1,26 @@
+int solution(int A[], int N) {
+    
+    // Initialize variables
+    int *count = (int *)malloc(N*sizeof(int));
+   
+    for(int i = 0; i < N; i++)
+       {
+           if(A[i] > 0 && A[i] <= N) 
+              {
+                  count[A[i] - 1] = 1; // If the positive integer exists in A, make the corresponding counter 1
+              }
+       }
+       
+    for(int i = 0; i < N; i++)
+       {
+           if(count[i] != 1) 
+              {
+                  return i + 1; // Return the first integer for which the counter is zero, since it is the smallest
+              }
+       }
+    
+    // Return result
+    free(count);
+    return N + 1; // If not counter is zero, return N + 1
+}
+
