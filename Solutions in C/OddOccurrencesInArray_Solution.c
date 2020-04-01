52909b47508ff0df4ad066553ad3c780048d1e23
Dimitra Kaitalidou <49205589+DimitraKaitalidou@users.noreply.github.com>
2020-04-01 18:49:04 +0300

    [ADDED] added OddOccurrencesInArray_Solution.c

---
+++ Solutions in C/OddOccurrencesInArray_Solution.c
@@ -0,0 +1,20 @@
+// Copyright 2020, Dimitra S. Kaitalidou, All rights reserved
+
+int comp (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+
+int solution(int A[], int N) {
+    
+   // Sort A
+   qsort(A, N, sizeof(int), comp);
+    
+   for(int i = 0; i < N - 1; i = i + 2)
+      {
+         // Return result
+         if(A[i] != A[i + 1]) return A[i];
+      }
+     
+   // Return result
+   return A[N - 1];
+}
