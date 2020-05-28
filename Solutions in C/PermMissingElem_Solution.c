a85e2136b58cab54bbcc1728bf780f950ae0f9b1
Dimitra Kaitalidou <49205589+DimitraKaitalidou@users.noreply.github.com>
2020-05-28 20:25:08 +0300

    [MODIFIED] removed spaces

--- Solutions in C/PermMissingElem_Solution.c
+++ Solutions in C/PermMissingElem_Solution.c
@@ -1,22 +1,22 @@
 // Copyright 2020, Dimitra S. Kaitalidou, All rights reserved
 
-int comp (const void * a, const void * b) {
+int comp (const void * a, const void * b){
    return ( *(int*)a - *(int*)b );
 }
 
-int solution(int A[], int N) {
+int solution(int A[], int N){
 
    // Sort A
    qsort(A, N, sizeof(int), comp);
-	
+
    // Check corner case, where 1 is missing
    if(A[0] != 1) return 1;
-    
+
    for(int i = 0; i < N - 1; i++)
       {
          if(A[i] != A[i + 1] - 1) return A[i] + 1;
       }
-    
+ 
    // Check corner case, where N + 1 is missing
    return N + 1;
 }
