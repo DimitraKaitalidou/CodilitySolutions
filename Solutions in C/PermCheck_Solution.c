81b7c3bf70b61ae1cb8ee2badca97d62ef431dce
Dimitra Kaitalidou <49205589+DimitraKaitalidou@users.noreply.github.com>
2020-05-28 20:24:12 +0300

    [MODIFIED] fixed indentation, removed spaces

--- Solutions in C/PermCheck_Solution.c
+++ Solutions in C/PermCheck_Solution.c
@@ -1,17 +1,17 @@
 // Copyright 2020, Dimitra S. Kaitalidou, All rights reserved
 
-int comp (const void * a, const void * b) {
+int comp (const void * a, const void * b){
    return ( *(int*)a - *(int*)b );
 }
 
-int solution(int A[], int N) {
-	
+int solution(int A[], int N){
+
    // Sort A
-	 qsort(A, N, sizeof(int), comp);
-	
+   qsort(A, N, sizeof(int), comp);
+
    // Check corner case, where 1 is missing
    if(A[0] != 1) return 0;
-    
+
    for(int i = 0; i < N - 1; i++)
       {
          if(A[i] != A[i + 1] - 1) return 0;
