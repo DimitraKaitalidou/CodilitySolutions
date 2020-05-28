27f23c00982ff58a74429ee72853f1de95f0ccb7
Dimitra Kaitalidou <49205589+DimitraKaitalidou@users.noreply.github.com>
2020-05-28 19:07:55 +0300

    [MODIFIED] fixed indentation, removed spaces

--- Solutions in C/MaxProductOfThree_Solution.c
+++ Solutions in C/MaxProductOfThree_Solution.c
@@ -1,21 +1,21 @@
 // Copyright 2019, Dimitra S. Kaitalidou, All rights reserved
 
-int comp (const void * a, const void * b) {
+int comp (const void * a, const void * b){
    return ( *(int*)a - *(int*)b );
 }
 
-int solution(int A[], int N) {
-    
-    // Sort A
-    qsort(A, N, sizeof(int), comp);
-    
-    if(A[0] < 0 && A[1] < 0 && A[N-1] > 0) 
-       {
-           if(A[0] * A[1] * A[N-1] > A[N-1] * A[N-2] * A[N-3]) return A[0] * A[1] * A[N-1];
-           else return A[N-1] * A[N-2] * A[N-3];
-       }
-    else if(A[0] < 0 && A[1] >= 0) return A[N-1] * A[N-2] * A[N-3];
-    else if(A[0] >= 0 || A[N-1] <= 0) return A[N-1] * A[N-2] * A[N-3];
-    else return -1;
+int solution(int A[], int N){
+
+   // Sort A
+   qsort(A, N, sizeof(int), comp);
+
+   if(A[0] < 0 && A[1] < 0 && A[N-1] > 0)
+      {
+         if(A[0] * A[1] * A[N-1] > A[N-1] * A[N-2] * A[N-3]) return A[0] * A[1] * A[N-1];
+         else return A[N-1] * A[N-2] * A[N-3];
+      }
+   else if(A[0] < 0 && A[1] >= 0) return A[N-1] * A[N-2] * A[N-3];
+   else if(A[0] >= 0 || A[N-1] <= 0) return A[N-1] * A[N-2] * A[N-3];
+   else return -1;
 }
 
