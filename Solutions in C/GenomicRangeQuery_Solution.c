f4ddf0e6c271a7e6d1d7f821cd3b3f5d23719693
Dimitra Kaitalidou <49205589+DimitraKaitalidou@users.noreply.github.com>
2020-05-28 18:47:21 +0300

    [MODIFIED] modified comment, removed spaces

--- Solutions in C/GenomicRangeQuery_Solution.c
+++ Solutions in C/GenomicRangeQuery_Solution.c
@@ -1,7 +1,7 @@
 // Copyright 2020, Dimitra S. Kaitalidou, All rights reserved
 
-struct Results solution(char *S, int P[], int Q[], int M) {
-    
+struct Results solution(char *S, int P[], int Q[], int M){
+
    // Initialize variables
    struct Results result;
    int N = strlen(S);
@@ -16,22 +16,22 @@ struct Results solution(char *S, int P[], int Q[], int M) {
       {
          if(i == 0)
             {
-               if(S[i] == 'A') {S_1[i] = 1; S_2[i] = 0; S_3[i] = 0; S_4[i] = 0;}
-               else if(S[i] == 'C') {S_1[i] = 0; S_2[i] = 1; S_3[i] = 0; S_4[i] = 0;}
-               else if(S[i] == 'G') {S_1[i] = 0; S_2[i] = 0; S_3[i] = 1; S_4[i] = 0;}
+               if(S[i] == 'A'){S_1[i] = 1; S_2[i] = 0; S_3[i] = 0; S_4[i] = 0;}
+               else if(S[i] == 'C'){S_1[i] = 0; S_2[i] = 1; S_3[i] = 0; S_4[i] = 0;}
+               else if(S[i] == 'G'){S_1[i] = 0; S_2[i] = 0; S_3[i] = 1; S_4[i] = 0;}
                else {S_1[i] = 0; S_2[i] = 0; S_3[i] = 0; S_4[i] = 1;}
             }
          else
             {
-               if(S[i] == 'A') {S_1[i] = S_1[i - 1] + 1; S_2[i] = S_2[i - 1]; S_3[i] = S_3[i - 1]; S_4[i] = S_4[i - 1];}
-               else if(S[i] == 'C') {S_1[i] = S_1[i - 1]; S_2[i] = S_2[i - 1] + 1; S_3[i] = S_3[i - 1]; S_4[i] = S_4[i - 1];}
-               else if(S[i] == 'G') {S_1[i] = S_1[i - 1]; S_2[i] = S_2[i - 1]; S_3[i] = S_3[i - 1] + 1; S_4[i] = S_4[i - 1];}
+               if(S[i] == 'A'){S_1[i] = S_1[i - 1] + 1; S_2[i] = S_2[i - 1]; S_3[i] = S_3[i - 1]; S_4[i] = S_4[i - 1];}
+               else if(S[i] == 'C'){S_1[i] = S_1[i - 1]; S_2[i] = S_2[i - 1] + 1; S_3[i] = S_3[i - 1]; S_4[i] = S_4[i - 1];}
+               else if(S[i] == 'G'){S_1[i] = S_1[i - 1]; S_2[i] = S_2[i - 1]; S_3[i] = S_3[i - 1] + 1; S_4[i] = S_4[i - 1];}
                else {S_1[i] = S_1[i - 1]; S_2[i] = S_2[i - 1]; S_3[i] = S_3[i - 1]; S_4[i] = S_4[i - 1] + 1;}
             }
       }
-   
-   // Check if the occurency of the nucleotides changed at point P, Q or between, starting with 
-   // the nucleotide with minimum impact factor   
+
+   // Check if the occurency of the nucleotides changed at point P, Q or between, starting with
+   // the nucleotide with minimum impact factor
    for(int j = 0; j < M; j++)
       {
          if(S_1[P[j]] != S_1[P[j] - 1] || S_1[Q[j]] != S_1[Q[j] - 1] || S_1[P[j]] != S_1[Q[j]]) A[j] = 1;
@@ -39,8 +39,8 @@ struct Results solution(char *S, int P[], int Q[], int M) {
          else if(S_3[P[j]] != S_3[P[j] - 1] || S_3[Q[j]] != S_3[Q[j] - 1] || S_3[P[j]] != S_3[Q[j]]) A[j] = 3;
          else A[j] = 4;
       }
-   
-   // Return the result
+
+   // Return result
    result.A = A;
    result.M = M;
    return result;
