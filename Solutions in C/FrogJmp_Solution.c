a2b2a7d1c4e7c189b3900edcb646c9c996c3e02d
Dimitra Kaitalidou <49205589+DimitraKaitalidou@users.noreply.github.com>
2020-05-28 18:40:04 +0300

    [MODIFIED] removed spaces

--- Solutions in C/FrogJmp_Solution.c
+++ Solutions in C/FrogJmp_Solution.c
@@ -1,14 +1,14 @@
 // Copyright 2019, Dimitra S. Kaitalidou, All rights reserved
 
-int solution(int X, int Y, int D) {
-    
+int solution(int X, int Y, int D){
+
    // Initialize variables
    int r_Y;
    int r;
    int jumps;
-	
+
    r_Y = Y - X; // Calculate the remaining distance
-    
+
    if(r_Y > D) // If the remaining distance is greater than D, divide the remaining distance with D
       {
          r = r_Y%D; 
@@ -20,7 +20,7 @@ int solution(int X, int Y, int D) {
          jumps = 1;
       }
    else jumps = 0; // If the remaining distance is zero, no jumps are required
-    
+
    // Return result
    return jumps;
 }
