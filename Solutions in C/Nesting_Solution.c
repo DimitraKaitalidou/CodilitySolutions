e556efaa61e8e2c35bcea0a4ea9ec6230289a9e9
Dimitra Kaitalidou <49205589+DimitraKaitalidou@users.noreply.github.com>
2020-09-08 15:26:10 +0300

    [ADDED] added Nesting_Solution.c

---
+++ Solutions in C/Nesting_Solution.c
@@ -0,0 +1,34 @@
+// Copyright 2020, Dimitra S. Kaitalidou, All rights reserved
+
+int solution(char *S){
+
+   // Initialize variables
+   int N = strlen(S);
+   int open = -1;
+   char *open_array = (char*)calloc(N,sizeof(char));
+
+   if(N % 2 != 0) return 0;
+
+   // Handle the parentheses as a stack, 
+   // i.e., last parenthesis that opens is the first to close (LIFO)
+   // Keep in mind that the ANSI codes for "(" and ")" are 40 and 41, respectively
+   for(int i = 0; i < N ; i++)
+      {
+         if(S[i] == '(') 
+            {
+               open++;
+               open_array[open] = S[i];
+            }
+         else if(S[i] == open_array[open] + 1)
+            {
+               open_array[open] = ' ';
+               open--;
+            }
+         else return 0;
+      }
+
+   // Return result
+   free(open_array);
+   if(open == -1) return 1;
+   else return 0;
+}
